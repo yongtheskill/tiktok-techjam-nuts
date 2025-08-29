ab7df47a36b46e61fd02c85aefff88d8bdcfa208
yongtheskill <yongee2003@gmail.com>
2025-08-29 22:39:50 +0800

    delete lynx

--- frontend/src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-const apiBase = 'http://10.45.16.69:8787';
-
-export async function fetchJson<T>(path: string, options?: RequestInit) {
-  const res = await fetch(apiBase + path, options);
-  if (!res.ok) {
-    throw new Error(`HTTP error! status: ${res.status}`);
-  }
-  return (await res.json()) as T;
-}
